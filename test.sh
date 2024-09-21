f08adefa8925b80e4b10d3fa7d4b0d5e6a770e43
waterlens <waterlens@waterlens.moe>
2024-09-22 00:50:42 +0800

    fix bugs

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 for file in test/test_src/*.mbt
   echo "Testing $file"
-  moon run src/bin/main.mbt -- --end-stage knf-opt $file
+  moon run src/bin/main.mbt -- --core-interpreter $file
   echo \n
 end
