2746f564939921b9085bee059973442e41a4fc5c
waterlens <waterlens@waterlens.moe>
2024-09-28 20:27:07 +0800

    Fix upstream tests

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 for file in test/test_src/*.mbt
   echo "Testing $file"
-  moon run src/bin/main.mbt -- --end-stage typecheck $file
+  moon run src/bin/main.mbt -- --closure-interpreter $file
 end
