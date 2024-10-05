f1acff01b7ba06c1b13018dc1d27ab419272a18d
waterlens <waterlens@waterlens.moe>
2024-10-05 18:55:19 +0800

    Lowering WIP

--- run-islec.sh
+++ run-islec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo "Generating src/lower/riscv/generated/lower.mbt" 
-islec src/core/prelude.isle src/core/core.isle src/riscv/riscv.isle src/lower/riscv/lower.isle > src/lower/riscv/generated/lower.mbt
+islec src/core/prelude.isle src/core/core.isle src/riscv/riscv.isle src/lower/riscv/lower.isle > src/lower/riscv/generated/generated.mbt
 echo "Done"
 
