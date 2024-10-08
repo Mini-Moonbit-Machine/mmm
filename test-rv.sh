201dfea674e6a6598a104942989e9642be715446
waterlens <waterlens@waterlens.moe>
2024-10-09 01:54:05 +0800

    Ugly workaround: 2 scratch registers

--- test-rv.sh
+++ test-rv.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env fish
 for file in test/test_src/*.mbt
-  rm out
-  if [ $file = "test/test_src/inprod-rec.mbt" ]
-    continue
-  end
-  if [ $file = "test/test_src/inprod.mbt" ]
-    continue
-  end
+  rm -f out
   echo "Testing $file"
   moon run -g src/bin/main.mbt -- --end-stage riscv $file > $file.S
   zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a $file.S -O Debug -fno-strip -mcpu=baseline_rv64
