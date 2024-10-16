d85c2946f681893a692f759c91225ad4059462f2
waterlens <waterlens@waterlens.moe>
2024-10-16 23:02:59 +0800

    Update

--- debug3.sh
+++ debug3.sh
@@ -3,6 +3,8 @@ set -e
 rm -f out
 moon run -g src/bin/main.mbt -- --end-stage riscv $1 > $1.S
 zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a $1.S -O Debug -fno-strip -mcpu=baseline_rv64
-scp ./out rvvm:~ 
+scp ./out rvvm:~
 scp $1.S rvvm:~
+scp ${1%.*}.in rvvm:~
+scp ${1%.*}.ans rvvm:~
 ssh rvvm
