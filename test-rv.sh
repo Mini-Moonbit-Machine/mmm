577a563a6ecf4ef8bc5b7619cf3968e1435c0913
waterlens <waterlens@waterlens.moe>
2024-10-18 19:50:39 +0800

    Try to fix some stuff

--- test-rv.sh
+++ test-rv.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env fish
+
 for file in test/test_src/*.mbt
-  if [ $file = "test/test_src/bf.mbt" ]
-    continue
-  end
   rm -f out
   echo "Testing $file"
-  moon run -g src/bin/main.mbt -- --end-stage riscv $file -o out.S
+  moon run src/bin/main.mbt -- --end-stage riscv -notc $file -o out.S
   zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a out.S -O Debug -fno-strip -mcpu=baseline_rv64
   ./rvlinux -n out
   echo \n
--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 for file in test/test_src/*.mbt
   echo "Testing $file"
-  moon run src/bin/main.mbt -- --end-stage riscv $file
+  moon run src/bin/main.mbt -- --end-stage riscv -notc $file
   echo \n
 end
