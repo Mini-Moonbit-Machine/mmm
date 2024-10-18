cf603ed4fca20d19a332119e3603d2019648808e
waterlens <waterlens@waterlens.moe>
2024-10-18 19:15:03 +0800

    Add trace system

--- test-rv.sh
+++ test-rv.sh
@@ -5,8 +5,8 @@ for file in test/test_src/*.mbt
   end
   rm -f out
   echo "Testing $file"
-  moon run -g src/bin/main.mbt -- --end-stage riscv $file > $file.S
-  zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a $file.S -O Debug -fno-strip -mcpu=baseline_rv64
+  moon run -g src/bin/main.mbt -- --end-stage riscv $file -o out.S
+  zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a out.S -O Debug -fno-strip -mcpu=baseline_rv64
   ./rvlinux -n out
   echo \n
   riscv64-elf-size out
