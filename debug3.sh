f124eacdaecb49a10db3845d531814e8cc0e263f
waterlens <waterlens@waterlens.moe>
2024-10-23 16:13:02 +0800

    Try to fix lifting

--- debug3.sh
+++ debug3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 rm -f out
-moon run -g src/bin/main.mbt -- --end-stage riscv $1 > $1.S
+moon run -g src/bin/main.mbt -- --end-stage riscv $1 -o $1.S
 zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a $1.S -O Debug -fno-strip -mcpu=baseline_rv64
 scp ./out rvvm:~
 scp $1.S rvvm:~
