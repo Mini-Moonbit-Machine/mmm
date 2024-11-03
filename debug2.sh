97739b358da40a9031b7da7cc6ae3085ad4df2a5
waterlens <waterlens@waterlens.moe>
2024-11-04 00:58:00 +0800

    Reduce size & fix bugs

--- debug2.sh
+++ debug2.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 rm -f out
-moon run -g src/bin/main.mbt -- --end-stage riscv $1 -o $1.S
-zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a $1.S -O Debug -fno-strip -mcpu=baseline_rv64
-./rvlinux -n out
+moon run -g src/bin/main.mbt -- --end-stage wasm $1 -o $1.wat
