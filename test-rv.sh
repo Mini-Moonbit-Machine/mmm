c2e02a48e9ade15f7a15c5625d4424f962a98277
waterlens <waterlens@waterlens.moe>
2024-11-09 19:25:30 +0800

    Add separate size log

--- test-rv.sh
+++ test-rv.sh
@@ -7,6 +7,4 @@ for file in test/test_src/*.mbt
   zig build-exe -target riscv64-linux -femit-bin=out riscv_rt/zig-out/lib/libmincaml.a out.S -O Debug -fno-strip -mcpu=baseline_rv64
   ./rvlinux -n out
   echo \n
-  riscv64-elf-size out
-  echo \n
 end
