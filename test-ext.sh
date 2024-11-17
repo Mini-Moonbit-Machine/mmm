b86606e7de15fc2e6e77bad0234656a6b10fefc3
waterlens <waterlens@waterlens.moe>
2024-11-17 21:33:32 +0800

    Add user-defined operators; Fix Js backend

--- test-ext.sh
+++ test-ext.sh
@@ -2,14 +2,12 @@
 
 for file in test/test_src/*.mbt
   echo "Testing $file"
-  moon run src/bin/main.mbt -- --ext -notc $file -o out.mjs
-  node js_rt/runtime.mjs out.mjs
+  moon run src/bin/main.mbt -- --ext -notc $file -o out.mjs && node js_rt/runtime.mjs out.mjs
   echo \n
 end
 
 for file in test/longer-cases/*.mbt
   echo "Testing $file"
-  moon run src/bin/main.mbt -- --ext -notc $file -o out.mjs
-  node js_rt/runtime.mjs out.mjs
+  moon run src/bin/main.mbt -- --ext -notc $file -o out.mjs && node js_rt/runtime.mjs out.mjs
   echo \n
 end
