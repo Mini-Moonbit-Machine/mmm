9df0b770d2c704c339959c46f07c25f92adec4c9
waterlens <waterlens@waterlens.moe>
2024-11-22 19:58:25 +0800

    Adjust test script

--- submit.sh
+++ submit.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 set -ex
-git archive -o "submit-$(git rev-parse --short HEAD)-$(python conf.py).zip" $1 ./src ./moon.mod.json ./mmm.json
+if [ "$#" -eq 0 ]; then
+  stash=$(git stash create)
+  filename="submit-$(git rev-parse --short $stash)-$(python conf.py).zip"
+  git archive -o $filename $stash ./src ./moon.mod.json ./mmm.json
+fi
+if [ "$#" -eq 1 ]; then
+  filename="submit-$(git rev-parse --short $1)-$(python conf.py).zip"
+  git archive -o $filename $1 ./src ./moon.mod.json ./mmm.json
+fi
