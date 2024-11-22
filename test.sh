b71589ef3181997993eb047c109fb57a31b2e50f
Zhu Siyuan <3200100491@zju.edu.cn>
2024-11-22 12:27:06 +0000

    Simd like 12 integer/double register fma mixed test

--- test.sh
+++ test.sh
@@ -11,3 +11,9 @@ for file in test/longer-cases/*.mbt
   moon run src/bin/main.mbt -- --end-stage riscv $file --use-xthead
   echo \n
 end
+
+for file in test/more-tests/*.mbt
+  echo "Testing $file"
+  moon run src/bin/main.mbt -- --end-stage riscv $file --use-xthead
+  echo \n
+end
